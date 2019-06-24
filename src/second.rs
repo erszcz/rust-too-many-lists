647404d60a8b0bc1b7adf6758af691b7b0c157da
Radek Szymczyszyn <lavrin@gmail.com>
2019-06-24 12:40:03 +0200

    Implement .peek() and .peek_mut() (end of 3.3)

--- src/second.rs
+++ src/second.rs
@@ -30,6 +30,18 @@ impl<T> List<T> {
         })
     }
 
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| {
+            &node.elem
+        })
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| {
+            &mut node.elem
+        })
+    }
+
 }
 
 impl<T> Drop for List<T> {
@@ -75,6 +87,26 @@ mod test {
         // Check exhaustion
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+        list.push(1); list.push(2); list.push(3);
+
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+        // Not like that! It's a pattern match.
+        //list.peek_mut().map(|&mut value| {
+        // This one actually leaves value as &mut for mutation.
+        list.peek_mut().map(|value| {
+            *value = 42;
+        });
+
+        assert_eq!(list.peek(), Some(&42));
+        assert_eq!(list.pop(), Some(42));
     }
 
 }
